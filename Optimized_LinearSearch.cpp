098f30db6ea60844cbb4fb31ba0133fd5d2f8eb3
RahulBaid99 <51899171+RahulBaid99@users.noreply.github.com>
2020-09-21 13:55:45 +0530

    Update Optimized_LinearSearch.cpp

--- Optimized_LinearSearch.cpp
+++ Optimized_LinearSearch.cpp
@@ -1,41 +1,6 @@
 #include <iostream>
 using namespace std;
-void Print(int p[], int n)
-{
-    for (int i=0;i<n;i++)
-    {
-        cout<<p[i]<<endl;
-    }
-}
- void Swapo(int *ptr1, int *ptr2 )
-    {
-
-        int temp;
-        temp = *ptr1;
-        *ptr1 = *ptr2;
-        *ptr2 = temp;
-
-    }
-void Linear_Search(int p[],int n)
-{
-    int x,found=0,j=0;
-    cout<<"Enter an element you wish to find"<<endl;
-    cin>>x;
-    for(j=0;j<n;j++)
-    {
-        if(p[j]==x){
-        found=1;
-        break;}}
-        if(found==1){
-        cout<<"element is at:  "<<j+1<<endl;
-        cout<<"Optimized Array:"<<endl;
-        Swapo(p+j,p+0);
-        Print(p,n);
-        }
-        else{cout<<"not Present";}
-
-    }
-
+void Linear_Search(int p[],int n);
 int main()
 {
     cout<<"Enter number of elements"<<endl;
@@ -47,7 +12,7 @@ int main()
         cout<<"Number of elements can't be 0!"<<endl<<"Try Again!!"<<endl;
     }
     else{
-    cout<<"enter "<<n<<" numbers"<<endl;
+        cout<<"enter "<<n<<" numbers"<<endl;
     for (int i=0;i<n;i++)
     {
         cin>>p[i];
@@ -55,4 +20,45 @@ int main()
     Linear_Search(p,n);
     delete []p;
     return 0;
+}
+
+void Print(int p[], int n)
+{
+    for (int i=0;i<n;i++)
+    {
+        cout<<p[i]<<endl;
+    }
+}
+
+ void Swapo(int *ptr1, int *ptr2 )
+    {
+       int temp;
+       temp = *ptr1;
+       *ptr1 = *ptr2;
+       *ptr2 = temp;
+    }
+
+void Linear_Search(int p[],int n)
+{
+    int x,y,found=0,j=0;
+    cout<<"Enter how many times you wish to find"<<endl;
+    cin>>y;
+    for(int k=0;k<y;k++){
+    cout<<"Enter an element you wish to find"<<endl;
+    cin>>x;
+    for(j=0;j<n;j++)
+    {
+        if(p[j]==x)
+        {
+            found=1;
+        break;}}
+        if(found==1)
+        {
+            cout<<"element is at:  "<<j+1<<endl;
+            cout<<"Optimized Array:"<<endl;
+            Swapo(p+j,p+0);
+            Print(p,n);
+        }
+        else{cout<<"not Present"<<endl;}
+    }
 }
