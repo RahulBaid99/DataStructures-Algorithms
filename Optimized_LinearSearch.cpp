b9279bb32cfe8b657a565f6dbde3b8ec52df6278
RahulBaid99 <51899171+RahulBaid99@users.noreply.github.com>
2020-09-22 12:08:04 +0530

    Update Optimized_LinearSearch.cpp

--- Optimized_LinearSearch.cpp
+++ Optimized_LinearSearch.cpp
@@ -25,15 +25,15 @@ void Print(int p[], int n)
 
 void Linear_Search(int p[],int n)
 {
-    int x,y,found=0,j=0;
+    int key,y,found=0,j=0;
     cout<<"Enter how many times you wish to find"<<endl;
     cin>>y;
     for(int k=0;k<y;k++){
     cout<<"Enter an element you wish to find"<<endl;
-    cin>>x;
+    cin>>key;
     for(j=0;j<n;j++)
     {
-        if(p[j]==x)
+        if(p[j]==key)
         {
             found=1;
         break;}}
@@ -61,7 +61,8 @@ void Array(){
     for (int i=0;i<n;i++)
     {
         cin>>p[i];
-    }}
+        }
+    }
     char z;
     cout<<"If you wish to search an element press 'y' else press'n' "<<endl;
     cin>>z;
